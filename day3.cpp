0d7790d11278da3338b30893b7980bde43696f58
vcarlsson <viktor.carlsson@vector.com>
2025-12-03 17:26:50 +0100

    Day 3 Part 2 solution

--- day3.cpp
+++ day3.cpp
@@ -1,4 +1,5 @@
 #include <cctype>
+#include <cmath>
 #include <cstddef>
 #include <fstream>
 #include <iostream>
@@ -7,37 +8,27 @@
 int main() {
   std::ifstream input("input/day3.txt");
   std::size_t sum{0};
+  static constexpr std::size_t kIterations{12};
   for (std::string line; std::getline(input, line);) {
-    char largest{'0'};
-    std::size_t joltage{0};
     std::size_t pos{0};
-    for (std::size_t i{0}; i < line.length() - 1; i++) {
-      if (!std::isdigit(line[i], std::locale{})) {
-        continue;
+    for (std::size_t iteration{0}; iteration < kIterations; iteration++) {
+      std::size_t const digits_remaining{kIterations - iteration - 1};
+      char largest{'0'};
+      for (std::size_t i{pos}; i < line.length() - digits_remaining; i++) {
+        if (!std::isdigit(line[i], std::locale{})) {
+          continue;
+        }
+        if (line[i] > largest) {
+          largest = line[i];
+          pos = i;
+        }
+        if (largest == '9') {
+          break;
+        }
       }
-      if (line[i] > largest) {
-        largest = line[i];
-        pos = i;
-      }
-      if (largest == '9') {
-        break;
-      }
+      sum += (largest - '0') * std::pow(10, digits_remaining);
+      pos++;
     }
-    joltage = (largest - '0') * 10;
-    largest = '0';
-    for (std::size_t i{pos + 1}; i < line.length(); i++) {
-      if (!std::isdigit(line[pos], std::locale{})) {
-        continue;
-      }
-      if (line[i] > largest) {
-        largest = line[i];
-      }
-      if (largest == '9') {
-        break;
-      }
-    }
-    joltage += largest - '0';
-    sum += joltage;
   }
   std::cout << "Sum: " << sum << std::endl;
   return 0;
