d60a4a869e82de272c7acd25141c68c2810186d0
vcarlsson <viktor.carlsson@vector.com>
2025-12-05 14:09:51 +0100

    Day 5 Part 2 solution

--- day5.cpp
+++ day5.cpp
@@ -1,7 +1,9 @@
+#include <algorithm>
 #include <cstddef>
 #include <cstdint>
 #include <fstream>
 #include <iostream>
+#include <set>
 #include <utility>
 #include <vector>
 
@@ -15,17 +17,41 @@ int main() {
       ranges.push_back(
           std::make_pair(std::stoll(line.substr(0, delimiter_pos)),
                          std::stoll(line.substr(delimiter_pos + 1))));
-    } else if (line != "") {
-      std::uint64_t id{static_cast<uint64_t>(std::stoll(line))};
+    } // else if (line != "") {
+    //   std::uint64_t id{static_cast<uint64_t>(std::stoll(line))};
 
-      for (auto range : ranges) {
-        if (id >= range.first && id <= range.second) {
-          count++;
-          break;
-        }
+    //   for (auto range : ranges) {
+    //     if (id >= range.first && id <= range.second) {
+    //       count++;
+    //       break;
+    //     }
+    //   }
+    // }
+  }
+  std::vector<std::pair<std::uint64_t, std::uint64_t>> ranges_without_overlap{};
+  std::sort(ranges.begin(), ranges.end());
+  for (auto range : ranges) {
+    for (auto range_no_overlap : ranges_without_overlap) {
+      if (range.first >= range_no_overlap.first &&
+          range.first <= range_no_overlap.second) {
+        range.first = range_no_overlap.second + 1;
+      }
+      if (range.second >= range_no_overlap.first &&
+          range.second <= range_no_overlap.second) {
+        range.second = range_no_overlap.first - 1;
       }
     }
+    if (range.first <= range.second) {
+      ranges_without_overlap.push_back(range);
+    }
   }
-  std::cout << count << std::endl;
+  std::uint64_t sum{0};
+  std::sort(ranges_without_overlap.begin(), ranges_without_overlap.end());
+  for (auto range_no_overlap : ranges_without_overlap) {
+    std::cout << range_no_overlap.first << " " << range_no_overlap.second
+              << std::endl;
+    sum += (range_no_overlap.second - range_no_overlap.first) + 1;
+  }
+  std::cout << sum << std::endl;
   return 0;
 }
